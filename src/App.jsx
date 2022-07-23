83e278ed07b3a122af15d7d87d9b70d8b146fef9
LMathers00 <lukemathers@hotmail.co.uk>
2022-07-23 17:14:03 +0100

    Completed set up for react app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello and welcome to jackass
+        </p>
+          Continue the code dumbass
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
